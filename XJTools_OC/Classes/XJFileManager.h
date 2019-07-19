094c3f6af3cc8df60f31e6c5af9b298259658baf
jingxuelong <jingxuelong0037@126.com>
2019-07-19 22:59:59 +0800

    1.添加部分路径以及文件大小计算

--- XJTools_OC/Classes/XJFileManager.h
+++ XJTools_OC/Classes/XJFileManager.h
@@ -16,12 +16,65 @@ NS_ASSUME_NONNULL_BEGIN
  Documents目录
  */
 @property (copy,nonatomic,readonly,class) NSString* documentsPath;
+
+/**
+ Library目录
+ */
 @property (copy,nonatomic,readonly,class) NSString* libraryPath;
+
+/**
+ Temp目录
+ */
 @property (copy,nonatomic,readonly,class) NSString* tempPath;
+
+/**
+ cache目录
+ */
 @property (copy,nonatomic,readonly,class) NSString* cachePath;
+
+/**
+ preferences目录
+ */
 @property (copy,nonatomic,readonly,class) NSString* preferencesPath;
 
 
+/**
+ 路径是否为文件夹
+
+ @param path  path
+ @return bool value
+ */
++ (BOOL)pathIsFolder:(NSString*)path;
+
+/**
+ 指定路径文件或者文件夹大小
+
+ @param path 文件或文件夹路径
+ @return size
+ */
++ (long long)fileSizeAtPath:(NSString*)path;
+
+/**
+ 清空Library 中cache文件夹
+
+ @return 是否成功
+ */
++ (BOOL)clearCache;
+
+/**
+ 当前cache文件夹size
+
+ @return  size
+ */
++ (long long)cacheDataSize;
+
+/**
+ 返回字符串格式size
+
+ @param size long long
+ @return NSString类型
+ */
++ (NSString*)changeStringSize:(long long)size;
 
 @end
 
--- XJTools_OC/Classes/XJFileManager.m
+++ XJTools_OC/Classes/XJFileManager.m
@@ -29,6 +29,55 @@
 }
 
 
++ (BOOL)pathIsFolder:(NSString*)path{
+    BOOL isFolder = NO;
+    [NSFileManager.defaultManager fileExistsAtPath:path isDirectory:&isFolder];
+    return isFolder;
+}
 
++ (BOOL)clearCache{
+    return [NSFileManager.defaultManager removeItemAtPath:[self cachePath] error:nil];
+}
+
++ (long long)cacheDataSize{
+    return [self fileSizeAtPath:[self cachePath]];
+}
+
++ (long long)fileSizeAtPath:(NSString*)path{
+    long long size = 0;
+    if ([self pathIsFolder:path]) {
+        NSEnumerator *childFilesEnumerator = [[NSFileManager.defaultManager subpathsAtPath:path] objectEnumerator];
+        NSString* fileName;        
+        while ((fileName = [childFilesEnumerator nextObject]) != nil){
+            NSString* fileAbsolutePath = [path stringByAppendingPathComponent:fileName];
+            size += [self singleFileSizeWith:fileAbsolutePath];
+        }
+    }else{
+        size = [self singleFileSizeWith:path];
+    }
+    return size;
+}
+
++ (NSString*)changeStringSize:(long long)size{
+    if (size < 1024.0) {
+        return  [NSString stringWithFormat:@"%.2fB",size * 1.0];
+    }else if (size >= 1024.0 && size < (1024.0*1024.0)){
+        return  [NSString stringWithFormat:@"%.2fKB",size/1024.0];
+    }if (size >= (1024.0*1024.0) && size < (1024.0*1024.0*1024.0)) {
+        return [NSString stringWithFormat:@"%.2fMB", size/(1024.0*1024.0)];
+    }else{
+        return [NSString stringWithFormat:@"%.2fGB", size/(1024.0*1024.0*1024.0)];
+    }
+}
+
+
+#pragma mark--<private menth>
++ (long long)singleFileSizeWith:(NSString*)path{
+    long long size = 0;
+    if ([NSFileManager.defaultManager fileExistsAtPath:path]){
+        size = [[NSFileManager.defaultManager attributesOfItemAtPath:path error:nil] fileSize];
+    }
+    return size;
+}
 
 @end
