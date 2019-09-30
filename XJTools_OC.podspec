0aacdff91248298622ae03cf89f8b7b32332aa83
jingxuelong <jingxuelong0037@126.com>
2019-09-30 21:36:07 +0800

    1.更新部分宏

--- XJTools_OC.podspec
+++ XJTools_OC.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'XJTools_OC'
-  s.version          = '0.1.3'
+  s.version          = '1.0.0'
   s.summary          = 'tools'
 
 # This description is used to generate tags and improve search results.
--- XJTools_OC/Classes/XJTools_OC.h
+++ XJTools_OC/Classes/XJTools_OC.h
@@ -9,12 +9,27 @@
 #ifndef XJTools_OC_h
 #define XJTools_OC_h
 
-#define XJ_Screen_Wide                          [UIScreen mainScreen].bounds.size.width
-#define XJ_Screen_Height                        [UIScreen mainScreen].bounds.size.height
+//高度值
+#define k_XJScreen_Wide                          [UIScreen mainScreen].bounds.size.width
+#define k_XJScreen_Height                        [UIScreen mainScreen].bounds.size.height
+#define k_XJStatusBarHeight       UIApplication.sharedApplication.statusBarFrame.size.height
+#define k_XJNavHeight             44.0
+#define k_XJBothNavAndStatusHeight    (k_XJStatusBarHeight+k_XJNavHeight)
+#define k_XJTabbarHeight          49.0
+#define k_XJTabbarSafeAreaHeight      is_XJIphoneWithSafeArea?34.0:0.0
+#define k_XJBothTabbarAndSafeAreaHeight          (k_XJTabbarHeight+k_XJTabbarSafeAreaHeight)
+
+
+#define is_XJIphone                  UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPhone?YES:NO
+#define is_XJIphoneWithSafeArea \
+({BOOL hasSafeArea = NO;\
+if (@available(iOS 11.0, *)) {\
+hasSafeArea = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0;\
+}\
+(hasSafeArea && is_XJIphone);})
 
 #define XJ_ColorFromRGB(rgbValue)               [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define XJ_ColorFromRGBWithA(rgbValue,A)        [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:A]
-
 
 
 #import <XJFileManager.h>
